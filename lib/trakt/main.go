6c61938046fba4e450a5c3ec565aef494a997bb8
Alex Standke <xanderstrike@gmail.com>
2018-12-12 20:14:09 -0800

    get episode object for scrobblin purposes

--- lib/trakt/main.go
+++ lib/trakt/main.go
@@ -42,16 +42,33 @@ func Handle(pr plex.PlexResponse) {
 
 // [{"type":"show","score":1000,"show":{"title":"Disenchantment","year":2018,"ids":{"trakt":126558,"slug":"disenchantment","tvdb":340234,"imdb":"tt5363918","tmdb":73021,"tvrage":null}}}]
 
-type ShowIds struct {
-	Trakt int
+type Ids struct {
+	Trakt  int    `json:"trakt"`
+	Tvdb   int    `json:"tvdb"`
+	Imdb   string `json:"imdb"`
+	Tmdb   int    `json:"tmdb"`
+	Tvrage int    `json:"tvrage"`
 }
 
 type Show struct {
-	Ids ShowIds
+	Ids Ids
 }
 
 type ShowInfo struct {
 	Show Show
+}
+
+// [{"season":1,"number":1,"title":"A Princess, an Elf, and a Demon Walk Into a Bar","ids":{"trakt":2907713,"tvdb":6699345,"imdb":"tt6561570","tmdb":1494890,"tvrage":0}
+type Episode struct {
+	Season int    `json:"season"`
+	Number int    `json:"number"`
+	Title  string `json:"title"`
+	Ids    Ids    `json:"ids"`
+}
+
+type Season struct {
+	Number   int
+	Episodes []Episode
 }
 
 func HandleShow(pr plex.PlexResponse) {
@@ -60,9 +77,35 @@ func HandleShow(pr plex.PlexResponse) {
 	re := regexp.MustCompile("thetvdb://(\\d*)/(\\d*)/(\\d*)")
 	showID := re.FindStringSubmatch(pr.Metadata.Guid)
 
+	url := fmt.Sprintf("https://api.trakt.tv/search/tvdb/%s?type=show", showID[1])
+
+	resp_body := makeRequest(url)
+	fmt.Println(string(resp_body))
+
+	var showInfo []ShowInfo
+	_ = json.Unmarshal(resp_body, &showInfo)
+
+	fmt.Println(showInfo[0].Show.Ids.Trakt)
+
+	url = fmt.Sprintf("https://api.trakt.tv/shows/%d/seasons?extended=episodes", showInfo[0].Show.Ids.Trakt)
+
+	resp_body = makeRequest(url)
+	var seasons []Season
+	_ = json.Unmarshal(resp_body, &seasons)
+
+	fmt.Printf("%+v\n", seasons)
+
+	sweetBytes, _ := json.Marshal(seasons[0].Episodes[0])
+	fmt.Printf(string(sweetBytes))
+}
+
+func HandleMovie(pr plex.PlexResponse) {
+	fmt.Println("handling movie")
+}
+
+func makeRequest(url string) []byte {
 	client := &http.Client{}
 
-	url := fmt.Sprintf("https://api.trakt.tv/search/tvdb/%s?type=show", showID[1])
 	req, _ := http.NewRequest("GET", url, nil)
 
 	req.Header.Add("Content-Type", "application/json")
@@ -74,19 +117,7 @@ func HandleShow(pr plex.PlexResponse) {
 
 	defer resp.Body.Close()
 	resp_body, _ := ioutil.ReadAll(resp.Body)
-
-	fmt.Println(resp.Status)
-	fmt.Println(string(resp_body))
-
-	var showInfo []ShowInfo
-	err = json.Unmarshal(resp_body, &showInfo)
-	handleErr(err)
-
-	fmt.Println(showInfo[0].Show.Ids.Trakt)
-}
-
-func HandleMovie(pr plex.PlexResponse) {
-	fmt.Println("handling movie")
+	return resp_body
 }
 
 func handleErr(err error) {
