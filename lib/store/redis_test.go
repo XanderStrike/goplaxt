fe0f39cf2918b91c0d5eaea5db5b41f399b9db83
Gavin Mogan <git@gavinmogan.com>
2019-03-04 20:59:26 -0800

    load and save tests

--- lib/store/redis_test.go
+++ lib/store/redis_test.go
@@ -9,15 +9,14 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestHelloWorld(t *testing.T) {
+func TestLoadingUser(t *testing.T) {
 	s, err := miniredis.Run()
 	if err != nil {
 		panic(err)
 	}
 	defer s.Close()
 
-	client := NewRedisClient(s.Addr())
-	store := NewRedisStore(client)
+	store := NewRedisStore(NewRedisClient(s.Addr()))
 
 	s.HSet("user:id123", "username", "halkeye")
 	s.HSet("user:id123", "access", "access123")
@@ -31,6 +30,36 @@ func TestHelloWorld(t *testing.T) {
 		RefreshToken: "refresh123",
 		Updated:      time.Date(2019, 02, 25, 0, 0, 0, 0, time.UTC),
 	})
+	actual, err := json.Marshal(store.GetUser("id123"))
+
+	assert.EqualValues(t, string(expected), string(actual))
+}
+
+func TestSavingUser(t *testing.T) {
+	s, err := miniredis.Run()
+	if err != nil {
+		panic(err)
+	}
+	defer s.Close()
+
+	store := NewRedisStore(NewRedisClient(s.Addr()))
+	originalUser := &User{
+		ID:           "id123",
+		Username:     "halkeye",
+		AccessToken:  "access123",
+		RefreshToken: "refresh123",
+		Updated:      time.Date(2019, 02, 25, 0, 0, 0, 0, time.UTC),
+		store:        store,
+	}
+
+	originalUser.save()
+
+	assert.Equal(t, s.HGet("user:id123", "username"), "halkeye")
+	assert.Equal(t, s.HGet("user:id123", "access"), "access123")
+	assert.Equal(t, s.HGet("user:id123", "refresh"), "refresh123")
+	assert.Equal(t, s.HGet("user:id123", "updated"), "02-25-2019")
+
+	expected, err := json.Marshal(originalUser)
 	actual, err := json.Marshal(store.GetUser("id123"))
 
 	assert.EqualValues(t, string(expected), string(actual))
