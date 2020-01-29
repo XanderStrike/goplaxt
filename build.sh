14eb67ec326ea1d25ab0a5f44738dee8318efd74
Alex Standke <xanderstrike@gmail.com>
2020-01-29 09:25:30 -0800

    remove build script since its done by dockerhub now

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# build binary
-docker run --rm -e GO111MODULE=on -v "$PWD":/go/src/github.com/xanderstrike/goplaxt -w /go/src/github.com/xanderstrike/goplaxt iron/go:dev go build -o goplaxt-docker
-
-# build docker image
-docker build -t xanderstrike/goplaxt .
-
-rm goplaxt-docker
