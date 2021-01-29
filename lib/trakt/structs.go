458892385f351834847d8db83a22d98fbe4a2a1c
CorentinB <corentin@archive.org>
2021-01-29 17:46:03 +0100

    Fix Go warnings

--- lib/trakt/structs.go
+++ lib/trakt/structs.go
@@ -1,5 +1,6 @@
 package trakt
 
+// Ids represent the IDs representing a media item accross the metadata providers
 type Ids struct {
 	Trakt  int    `json:"trakt"`
 	Tvdb   int    `json:"tvdb"`
@@ -8,14 +9,17 @@ type Ids struct {
 	Tvrage int    `json:"tvrage"`
 }
 
+// Show represent a show's IDs
 type Show struct {
 	Ids Ids
 }
 
+// ShowInfo represent a show
 type ShowInfo struct {
 	Show Show
 }
 
+// Episode represent an episode
 type Episode struct {
 	Season int    `json:"season"`
 	Number int    `json:"number"`
@@ -23,26 +27,31 @@ type Episode struct {
 	Ids    Ids    `json:"ids"`
 }
 
+// Season represent a season
 type Season struct {
 	Number   int
 	Episodes []Episode
 }
 
+// Movie represent a movie
 type Movie struct {
 	Title string `json:"title"`
 	Year  int    `json:"year"`
 	Ids   Ids    `json:"ids"`
 }
 
+// MovieSearchResult represent a search result for a movie
 type MovieSearchResult struct {
 	Movie Movie
 }
 
+// ShowScrobbleBody represent the scrobbling status for a show
 type ShowScrobbleBody struct {
 	Episode  Episode `json:"episode"`
 	Progress int     `json:"progress"`
 }
 
+// MovieScrobbleBody represent the scrobbling status for a movie
 type MovieScrobbleBody struct {
 	Movie    Movie `json:"movie"`
 	Progress int   `json:"progress"`
