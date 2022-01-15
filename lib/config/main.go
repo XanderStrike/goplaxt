356721259ebfa63ebec7d7266528f647b3f4b212
Lukas Spieß <lukas.spiess@gmail.com>
2022-01-15 02:56:28 +0100

    Add ability to read config value from a file

--- lib/config/main.go
+++ lib/config/main.go
@@ -12,6 +12,14 @@ var TraktClientSecret string = getConfig("TRAKT_SECRET")
 func getConfig(name string) string {
 	if os.Getenv(name) != "" {
 		return os.Getenv(name)
+	} else if os.Getenv(name+"_FILE") != "" {
+		file, err := ioutil.ReadFile(os.Getenv(name + "_FILE"))
+
+		if err != nil {
+			panic(err)
+		}
+
+		return strings.TrimSpace(string(file))
 	}
 
 	return ""
